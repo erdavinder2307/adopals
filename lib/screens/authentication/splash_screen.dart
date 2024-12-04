475409b80ad2dfaf5e0cbccab0e7211fb42e30b1
Davinder Pal <davinder@solidevelectrosoft.com>
2024-12-04 09:55:38 +0530

    Refactor SplashScreen layout and improve image handling

--- lib/screens/authentication/splash_screen.dart
+++ lib/screens/authentication/splash_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFFA9FF), // Background color
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255), // Background color
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               "Find",
               style: TextStyle(
                 fontSize: 40,
@@ -17,70 +19,115 @@ class SplashScreen extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 10),
+            const Text(
               "Your Perfect Pet Companion!",
               style: TextStyle(
                 fontSize: 18,
                 color: Color(0xFF8B5EA9),
               ),
             ),
-            SizedBox(height: 30),
+            const SizedBox(height: 30),
             Stack(
-              alignment: Alignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 100,
-                  backgroundColor:
-                      Colors.pinkAccent[100], // Circle background color
+
+                alignment: Alignment.center,
+                children: [
+                const CircleAvatar(
+                  radius: 200,
+                  backgroundColor: Color.fromARGB(255, 255, 171, 171), // Transparent background color
                 ),
                 Positioned(
-                  top: 20,
-                  left: 50,
+                  top: 0,
+                  left: 60,
                   child: CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage('assets/images/puppy.jpg'),
+                  radius: 30,
+                  backgroundColor: Colors.transparent, // Transparent background color
+                  foregroundImage: const AssetImage('assets/images/puppy.jpg'),
+                  child: ClipOval(
+                    child: Image.asset(
+                    'assets/images/puppy.jpg',
+                    fit: BoxFit.cover,
+                    width: 60,
+                    height: 60,
+                    ),
+                  ),
                   ),
                 ),
                 Positioned(
-                  top: 20,
-                  right: 50,
+                  top: 0,
+                  right: 60,
                   child: CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage('assets/images/parrot.jpg'),
+                  radius: 30,
+                  backgroundColor: Colors.transparent, // Transparent background color
+                  foregroundImage: const AssetImage('assets/images/parrot.jpg'),
+                  child: ClipOval(
+                    child: Image.asset(
+                    'assets/images/parrot.jpg',
+                    fit: BoxFit.cover,
+                    width: 60,
+                    height: 60,
+                    ),
+                  ),
                   ),
                 ),
                 Positioned(
-                  bottom: 20,
-                  left: 50,
+                  top: 90,
+                  left: 0,
                   child: CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage('assets/images/small-fish.jpg'),
+                  radius: 30,
+                  backgroundColor: Colors.transparent, // Transparent background color
+                  foregroundImage: const AssetImage('assets/images/small-fish.jpg'),
+                  child: ClipOval(
+                    child: Image.asset(
+                    'assets/images/small-fish.jpg',
+                    fit: BoxFit.cover,
+                    width: 60,
+                    height: 60,
+                    ),
+                  ),
                   ),
                 ),
                 Positioned(
-                  bottom: 20,
-                  right: 50,
+                  top: 90,
+                  right: 0,
                   child: CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage('assets/images/baby-cat.jpg'),
+                  radius: 30,
+                  backgroundColor: Colors.transparent, // Transparent background color
+                  foregroundImage: const AssetImage('assets/images/baby-cat.jpg'),
+                  child: ClipOval(
+                    child: Image.asset(
+                    'assets/images/baby-cat.jpg',
+                    fit: BoxFit.cover,
+                    width: 60,
+                    height: 60,
+                    ),
+                  ),
                   ),
                 ),
                 Positioned(
+                  bottom: 0,
                   child: CircleAvatar(
-                    radius: 40,
-                    backgroundImage:
-                        AssetImage('assets/images/woman.jpg'), // Center image
+                  minRadius: 120,
+                  backgroundColor: Colors.transparent, // Transparent background color
+                  child: ClipOval(
+                    child: Image.asset(
+                    'assets/images/woman.png',
+                    fit: BoxFit.scaleDown,
+                    width: 330,
+                    height: 320,
+                    ),
+                  ),
                   ),
                 ),
-              ],
+                ],
+              
             ),
-            SizedBox(height: 40),
+            const SizedBox(height: 40),
             GestureDetector(
               onTap: () {
                 // Add navigation action here
               },
-              child: CircleAvatar(
+              child: const CircleAvatar(
                 radius: 25,
                 backgroundColor: Color(0xFF8B5EA9),
                 child: Icon(
